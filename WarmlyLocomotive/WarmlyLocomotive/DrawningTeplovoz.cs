a3b00db2671e6a0a77e887de2c4805e93b501d7a
ПИбд-23 Коптев Никита <132771164+nikitosPapirosss@users.noreply.github.com>
2023-10-26 18:54:51 +0400

    Update DrawningTeplovoz.cs

--- WarmlyLocomotive/WarmlyLocomotive/DrawningTeplovoz.cs
+++ WarmlyLocomotive/WarmlyLocomotive/DrawningTeplovoz.cs
@@ -9,51 +9,23 @@ namespace WarmlyLocomotive
 {
     public class DrawningTeplovoz
     {
-        /// <summary>
-        /// Класс-сущность
-        /// </summary>
         public EntityTeplovoz? EntityTeplovoz { get; private set; }
-        /// <summary>
-        /// Ширина окна
-        /// </summary>
+        
         private int _pictureWidth;
-        /// <summary>
-        /// Высота окна
-        /// </summary>
+        
         private int _pictureHeight;
-        /// <summary>
-        /// Левая координата прорисовки автомобиля
-        /// </summary>
+        
         private int _startPosX;
-        /// <summary>
-        /// Верхняя кооридната прорисовки автомобиля
-        /// </summary>
+        
         private int _startPosY;
-        /// <summary>
-        /// Ширина прорисовки автомобиля
-        /// </summary>
+        
         private readonly int _teplovozWidth = 105;
-        /// <summary>
-        /// Высота прорисовки автомобиля
-        /// </summary>
+        
         private readonly int _teplovozHeight = 37;
-        /// <summary>
-        /// Инициализация свойств
-        /// </summary>
-        /// <param name="speed">Скорость</param>
-        /// <param name="weight">Вес</param>
-        /// <param name="bodyColor">Цвет кузова</param>
-        /// <param name="additionalColor">Дополнительный цвет</param>
-        /// <param name="bodyKit">Признак наличия обвеса</param>
-        /// <param name="wing">Признак наличия антикрыла</param>
-        /// <param name="sportLine">Признак наличия гоночной полосы</param>
-        /// <param name="width">Ширина картинки</param>
-        /// <param name="height">Высота картинки</param>
-        /// <returns>true - объект создан, false - проверка не пройдена,
+        
         public bool Init(int speed, double weight, Color bodyColor, Color
         additionalColor, int width, int height)
         {
-            // TODO: Продумать проверки
             if (_teplovozWidth > width || _teplovozHeight > height)
             {
                 return false;
@@ -67,14 +39,9 @@ namespace WarmlyLocomotive
                 return true;
             }
         }
-        /// <summary>
-        /// Установка позиции
-        /// </summary>
-        /// <param name="x">Координата X</param>
-        /// <param name="y">Координата Y</param>
+        
         public void SetPosition(int x, int y)
         {
-            // TODO: Изменение x, y
             if(x<_pictureWidth && y < _pictureHeight && x>15 & y>30)
             {
                 _startPosX = x;
@@ -88,10 +55,7 @@ namespace WarmlyLocomotive
 
             }
         }
-        /// <summary>
-        /// Изменение направления перемещения
-        /// </summary>
-        /// <param name="direction">Направление</param>
+        
         public void MoveTransport(DirectionType direction)
         {
             if (EntityTeplovoz == null)
@@ -100,28 +64,24 @@ namespace WarmlyLocomotive
             }
             switch (direction)
             {
-                //влево
                 case DirectionType.Left:
                     if (_startPosX - EntityTeplovoz.Step > 0)
                     {
                         _startPosX -= (int)EntityTeplovoz.Step;
                     }
                     break;
-                //вверх
                 case DirectionType.Up:
                     if (_startPosY - EntityTeplovoz.Step > 0)
                     {
                         _startPosY -= (int)EntityTeplovoz.Step;
                     }
                     break;
-                // вправо
                 case DirectionType.Right:
                     if (_startPosX + EntityTeplovoz.Step < _pictureWidth-_teplovozWidth)
                     {
                         _startPosX += (int)EntityTeplovoz.Step;
                     }
                     break;
-                //вниз
                 case DirectionType.Down:
                     if (_startPosY + EntityTeplovoz.Step < _pictureHeight-_teplovozHeight)
                     {
