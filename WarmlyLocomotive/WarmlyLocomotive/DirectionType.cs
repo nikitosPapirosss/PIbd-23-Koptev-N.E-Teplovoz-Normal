9e44007e19a5d95a7f2de6d7d55c769050238623
ПИбд-23 Коптев Никита <132771164+nikitosPapirosss@users.noreply.github.com>
2023-10-26 18:52:55 +0400

    Update DirectionType.cs

--- WarmlyLocomotive/WarmlyLocomotive/DirectionType.cs
+++ WarmlyLocomotive/WarmlyLocomotive/DirectionType.cs
@@ -8,21 +8,9 @@ namespace WarmlyLocomotive
 {
     public enum DirectionType
     {
-        /// <summary>
-        /// Вверх
-        /// </summary>
         Up = 1,
-        /// <summary>
-        /// Вниз
-        /// </summary>
         Down = 2,
-        /// <summary>
-        /// Влево
-        /// </summary>
         Left = 3,
-        /// <summary>
-        /// Вправо
-        /// </summary>
         Right = 4
     }
 }
